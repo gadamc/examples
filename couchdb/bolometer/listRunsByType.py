907d7bf257f9de892b94213a2ad3ce2f96632308
Adam Cox <gadamc@gmail.com>
2011-06-01 17:24:23 +0200

    adding list runs by type.

---
+++ couchdb/bolometer/listRunsByType.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from couchdbkit import Server, Database
+import sys
+
+def main(*argv):
+  '''
+    This script queries the edwdb database (on edwdbik.fzk.de) and returns the runs
+    of a particular type. This script can also list the types of runs that are available
+    
+    Here are the examples for each one
+    
+    1) ./listRunsByType.py "calibration gamma"
+    2) ./listRunsByType.py list
+    
+  '''
+    
+  s = Server('http://edwdbik.fzk.de:5984')
+  db = s['edwdb']
+        
+  if len(argv) == 0:
+    print 'You need to supply an argument'
+    help(main)
+    return 
+      
+  arg1 = argv[0]
+    
+    
+  if arg1 == 'list':
+    vr = db.view('bolorunconfig/condition', group_level=1)
+    print 'Run Types'
+    for row in vr:
+      print row['key'][0]
+  else:
+    print 'looking for runs of type', arg1
+    vr = db.view('bolorunconfig/condition', endkey = [arg1,'',0], startkey = [arg1+'\ufff0', {}], group_level = 2, descending = True)
+    
+    for row in vr:
+      print row['key'][1]
+    
+    
+if __name__ == '__main__':
+  main(*sys.argv[1:])
