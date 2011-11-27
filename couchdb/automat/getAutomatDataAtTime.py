e70375564e7890632907095ba8a0987b378aff45
Adam Cox <adam.cox@kit.edu>
2011-11-27 15:27:30 +0100

    adding ignore list and adding more options so that the user can specify the database server and database name

--- couchdb/automat/getAutomatDataAtTime.py
+++ couchdb/automat/getAutomatDataAtTime.py
@@ -28,9 +28,15 @@ def main(*argv):
   '''
     Use this script to return the value of a particular cyrogenic variable at a particular time.
     
-    usage: $> python getAutomateDataAtTime.py [variable name] [-pctime VAL optional]  [year] [month] [day] [hour] 
-                                              [minute] [second] [microsecond (optional. = 0 by default)] [-method=name (optional, default is 'in')]
-                                              [-t=seconds (optional, default is 60 seconds)]
+    usage: $> python getAutomateDataAtTime.py [variable name] [-pctime VAL optional]  
+                                        [year] [month] [day] [hour] 
+                                        [minute] [second] [microsecond (optional. = 0 by default)] 
+                                        [-method=name (optional, default is 'in')]
+                                        [-t=seconds (optional, default is 60 seconds)]
+                                        [-server=<address> (optional, 
+                                                where address is 'http://dnsname:portNumber')]
+                                        [-db=name (optional, default is 'automat')]
+    
     
            examples: 
                     python getAutomatDataAtTime.py T_Speer 2011 10 27 0 10 10 0
@@ -38,22 +44,28 @@ def main(*argv):
                     python getAutomatDataAtTime.py T_Bolo -pctime 1321101265.017829
                     python getAutomatDataAtTime.py T_Bolo -pctime 1321101265.017829 -t=1000 -method=cn
 
+    
            $> python getAutomateDataAtTime.py list
-                This will return a list of all cryogenic variables that are available according to the latest documents written 
-                to the database. This doesn't necessarily mean that these cryogenic variables are available in all past data, but 
-                that is probably a reasonable assumption since the cryogenic data server does not change often.
+                This will return a list of all cryogenic variables that are available according to the 
+                latest documents writtento the database. This doesn't necessarily mean that these 
+                cryogenic variables are available in all past data, but that is probably a reasonable
+                assumption since the cryogenic data server does not change often.
            
     The time is always UTC time. 
     
     [optional]
-    -pctime [number]    Instead of passing in the year/month/day/hour/minutes/second/microsecond, you can pass in the PC time (which is)
+    -pctime [number]    Instead of passing in the year/month/day/hour/minutes/second/microsecond, 
+                        you can pass in the PC time (which is)
     
     methods:
     -method=bn
-          bn == "both neighbors". this will return the value of the cryogenic variable at both neighboring times
+          bn == "both neighbors". this will return the value of the cryogenic variable at both 
+                                  neighboring times
     -method=in
-          in == 'interpolation'. This computes the interpolated value between neighboring measurements. If this method fails because one of the nearby event times is outside of range (set by the
-          -t=seconds option), then the calculation is redone using the -method=cn switch. 
+          in == 'interpolation'. This computes the interpolated value between neighboring measurements. 
+                                 If this method fails because one of the nearby event times is 
+                                 outside of range (set by the -t=seconds option), then the calculation 
+                                 is redone using the -method=cn switch. 
     -method=en
           en == "early neighbor". This returns the neighbor value measured prior to the time you provide     
     -method=ln
@@ -62,18 +74,17 @@ def main(*argv):
           cn == "closest neighbor". This returns the neighbor value thas was measured closest to time you provide
                 
     time threshold:
-      -t=seconds.   If the time you choose is further away than some time 'seconds' from the nearest measurement, this function will return a message to stderr and nothing to stdout
+      -t=seconds.   Searches around your given event time by a range of +- seconds. If no data is found 
+                    in this range, nothing is printed to stdout and an error message is printed to stderr
         
+    select database server
+      -server=address   Default: address = https://edwdbik.fzk.de:6984. If you're using a local CouchDB,
+                        you probably want address = http://127.0.0.1:5984
+    
+    select database name
+      -db=name          Default: automat. 
   '''
 
-  
-  s = Server('https://edwdbik.fzk.de:6984')
-  db = s['automat']
-  
-  if argv[0] == 'list':
-    listVars(s,db)
-    return
-  
   cryoKey = argv[0]
   
   #build the eventTime from the arguments
@@ -86,17 +97,23 @@ def main(*argv):
       eventTime += datetime.timedelta(microseconds=float(argv[7]))
   
   #step back through the arguments to see if the user wants specific options
-  global theMethod, timeThreshold
+  global theMethod, timeThreshold, servName, dbName
   theMethod = 'en'
   timeThreshold = 60
+  servName = 'https://edwdbik.fzk.de:6984'
+  dbName = 'automat'
   def searchBack(*argv):
-    global theMethod, timeThreshold
+    global theMethod, timeThreshold, servName, dbName
     
     lastArg = argv[len(argv)-1]
     if lastArg.startswith('-t'):
       timeThreshold =  float(lastArg.split('=')[1])
     elif lastArg.startswith('-method'):
       theMethod = lastArg.split('=')[1]
+    elif lastArg.startswith('-server'):
+      servName = lastArg.split('=')[1]
+    elif lastArg.startswith('-db'):
+      dbName = lastArg.split('=')[1]
     else:
       #there are no more recognized args to look at
       return
@@ -106,7 +123,15 @@ def main(*argv):
   
   #
   searchBack(*argv)
+  
+  s = Server(servName)
+  db = s[dbName]
     
+  if argv[0] == 'list':
+    listVars(s,db)
+    return
+    
+  
   #determine the range of times over which we'll query the database
   startTime = eventTime - datetime.timedelta(seconds=timeThreshold)
   endTime = eventTime + datetime.timedelta(seconds=timeThreshold)
@@ -181,7 +206,7 @@ def main(*argv):
     
     #compute the time difference in seconds directly because the timedelta object method "totalseconds"
     #was implemented in python 2.7. Mac OS 10.6 ships with python 2.6 and so this method doesn't exist on those
-    #running Snow Leopard or sooner. 
+    #running Snow Leopard or older. 
     td = eventTime - earlyDatTime
     eventToEarly = (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
     td = lateDatTime - earlyDatTime
